445bfabf3e67064d506fae863c27894c6ed476bf
胡琤波 <657225485@qq.com>
2024-09-13 19:05:45 +0800

    types

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+console.log('开始编译过程...');
+console.log('正在导出类型...');
 const Redis78_1 = require("./Redis78");
+console.log('类型导出完成');
+console.log('编译过程结束');
 exports.default = Redis78_1.default;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+console.log('开始编译过程...');
 console.log('正在导出类型...');
 
 import Redis78 from "./Redis78";
 
 console.log('类型导出完成');
+console.log('编译过程结束');
 
 export default Redis78;
