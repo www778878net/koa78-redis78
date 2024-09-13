3c59cf0ae3178cfcec9b2e87189c353aaef71ca9
胡琤波 <657225485@qq.com>
2024-09-13 19:17:08 +0800

    test ok

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-console.log('开始编译过程...');
-console.log('正在清理 dist 目录...');
-console.log('正在导出类型...');
+
 
 import Redis78 from "./Redis78";
 
-console.log('类型导出完成');
-console.log('编译过程结束');
+
 
 export default Redis78;
